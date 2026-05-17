7ea1d5f93fbf1dfeeefcc68320e7da43bb6e5c86
lakshyacivilservices <palratiraj@gmail.com>
2026-05-18 00:20:56 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Lakshya Civil Services Academy",
+  description: "Quiz Platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
