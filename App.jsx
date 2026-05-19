0756a682f3cd922fa58af6412190f3b8cd42667a
lakshyacivilservices <palratiraj@gmail.com>
2026-05-20 02:42:25 +0530

    Create App component for Lakshya Quiz Academy

--- App.jsx
+++ App.jsx
@@ -1,1 +1,17 @@
 
+export default function App() {
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>📚 Lakshya Quiz Academy</h1>
+
+      <h2>Ancient History MCQ</h2>
+
+      <p>1. Who founded Maurya Empire?</p>
+
+      <button>Ashoka</button><br/><br/>
+      <button>Chandragupta Maurya ✅</button><br/><br/>
+      <button>Harsha</button><br/><br/>
+      <button>Bindusara</button>
+    </div>
+  );
+}
