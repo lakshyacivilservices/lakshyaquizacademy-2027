f87d8e5cc18031c0adeaec1275eec28f6652a0b4
lakshyacivilservices <palratiraj@gmail.com>
2026-05-20 16:24:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,35 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        background: "#071028",
+        minHeight: "100vh",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "72px",
+        }}
+      >
+        🚀 Lakshya Academy
+      </h1>
+
+      <p
+        style={{
+          fontSize: "28px",
+          marginTop: "20px",
+        }}
+      >
+        UPPSC • UGC NET • AI • Data Science
+      </p>
+    </main>
+  );
+}
+
 
